2692803c0c771170bfb21875cb74a8f710df9f9f
Edvard Makhlin <edvard.makhlin@iii.com>
2020-02-06 12:27:55 +0300

    [fix-pub-export] Fixed export of xxh3

--- fasthash/src/lib.rs
+++ fasthash/src/lib.rs
@@ -100,6 +100,7 @@ pub use crate::spooky::{Hasher128 as SpookyHasherExt, Hasher64 as SpookyHasher};
 pub use crate::t1ha::{t1ha0, t1ha1, t1ha2};
 pub use crate::t1ha2::{Hasher128 as T1haHasherExt, Hasher128 as T1haHasher};
 pub use crate::xx::Hasher64 as XXHasher;
+pub use crate::xxh3::{Hasher64 as XXH3Hasher, Hasher128 as XXH3HasherExt};
 cfg_if! {
     if #[cfg(target_pointer_width = "64")] {
         pub use crate::murmur2::Hasher64_x64 as Murmur2Hasher;
